c250639a93e84959ceeedb4ccd3bc7d0b32dbe04
solimander <solimander-dev@protonmail.com>
2023-08-21 15:56:41 -0600

    Remove unused code

--- packages/protocol/contracts/RioLRTAssetManager.sol
+++ packages/protocol/contracts/RioLRTAssetManager.sol
@@ -1,17 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.21;
 
-import {IRioLRTController} from './interfaces/IRioLRTController.sol';
+import {Clone} from '@solady/utils/Clone.sol';
 
-contract RioLRTAssetManager {
-    /// @notice Thrown when the caller is not the controller of the pool with the given ID.
-    error NOT_CONTROLLER_FOR_POOL(bytes32 poolId);
-
-    /// @notice Throws if called by any account other than the controller of the pool with the given ID.
-    modifier onlyController(bytes32 poolId) {
-        if (poolId != IRioLRTController(msg.sender).pool().getPoolId()) {
-            revert NOT_CONTROLLER_FOR_POOL(poolId);
-        }
-        _;
-    }
-}
+contract RioLRTAssetManager is Clone {}
