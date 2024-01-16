6dbbe51b6c8affaa9df0d6d456f3663dd20376b5
solimander <solimander-dev@protonmail.com>
2024-01-16 08:47:11 -0700

    Exchange rates

--- packages/subgraph/src/coordinator.ts
+++ packages/subgraph/src/coordinator.ts
@@ -1,6 +1,8 @@
 import { Deposited } from '../generated/templates/Coordinator/RioLRTCoordinator';
-import { Asset, Coordinator, Deposit } from '../generated/schema';
+import { Asset, Coordinator, Deposit, LiquidRestakingToken } from '../generated/schema';
 import { findOrCreateUser, toUnits } from './helpers/utils';
+import { BigDecimal } from '@graphprotocol/graph-ts';
+import { ETH_ADDRESS } from './helpers/constants';
 
 export function handleDeposited(event: Deposited): void {
   const coordinator = Coordinator.load(event.address.toHex())!;
@@ -11,13 +13,18 @@ export function handleDeposited(event: Deposited): void {
   const amountInUnits = toUnits(event.params.amountIn.toBigDecimal(), assetIn.decimals as u8);
   const amountOutUnits = toUnits(event.params.amountOut.toBigDecimal());
 
+  const restakingToken = LiquidRestakingToken.load(coordinator.restakingToken)!;
+  restakingToken.exchangeRateETH = getExchangeRateETH(assetIn, amountInUnits, amountOutUnits);
+  restakingToken.exchangeRateUSD = getExchangeRateUSD(assetIn, restakingToken.exchangeRateETH, assetIn.latestUSDPrice);
+  restakingToken.save();
+
   const deposit = new Deposit(`${event.transaction.hash.toHex()}-${event.logIndex.toString()}`);
   deposit.user = user.id;
   deposit.sender = event.params.user;
   deposit.assetIn = assetIn.id;
   deposit.amountIn = amountInUnits;
   deposit.amountOut = amountOutUnits;
-  deposit.restakingToken = coordinator.restakingToken;
+  deposit.restakingToken = restakingToken.id;
   deposit.userBalanceBefore = user.balance;
   deposit.userBalanceAfter = deposit.userBalanceBefore.plus(amountOutUnits);
   deposit.timestamp = event.block.timestamp;
@@ -32,4 +39,18 @@ export function handleDeposited(event: Deposited): void {
   user.save();
 
   deposit.save();
+}
+
+function getExchangeRateETH(asset: Asset, amountIn: BigDecimal, amountOut: BigDecimal): BigDecimal | null {
+  if (asset.id == ETH_ADDRESS) {
+    return amountIn.div(amountOut);
+  }
+  return null;
+}
+
+function getExchangeRateUSD(asset: Asset, exchangeRateETH: BigDecimal | null, price: BigDecimal | null): BigDecimal | null {
+  if (asset.id == ETH_ADDRESS && exchangeRateETH && price) {
+    return exchangeRateETH.times(price); 
+  }
+  return null;
 }
