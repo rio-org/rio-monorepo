d0f6419339ef8c3e1dd91f39843e2aa2b64cb30a
tlip <thomas.lipari@gmail.com>
2024-01-29 08:37:04 -0800

    feat: [RIO-85] add tos modal before connecting wallet

--- packages/ui/components/Shared/RainbowKitDisclaimer.tsx
+++ packages/ui/components/Shared/RainbowKitDisclaimer.tsx
@@ -1,8 +1,28 @@
-import { DisclaimerComponent } from '@rainbow-me/rainbowkit';
+export enum RainbowKitDisclaimerType {
+  CONNECTING = 'CONNECTING',
+  CHECKING = 'CHECKING'
+}
 
-export const RainbowKitDisclaimer: DisclaimerComponent = ({ Text, Link }) => (
+export const RainbowKitDisclaimer = ({
+  Text,
+  Link,
+  action = RainbowKitDisclaimerType.CONNECTING
+}: {
+  Text: React.FunctionComponent<{
+    children: React.ReactNode;
+  }>;
+  Link: React.FunctionComponent<{
+    children: React.ReactNode;
+    href: string;
+  }>;
+  action?: RainbowKitDisclaimerType;
+}) => (
   <Text>
-    By connecting your wallet, you agree to the{' '}
+    By{' '}
+    {action === RainbowKitDisclaimerType.CONNECTING
+      ? 'connecting your wallet'
+      : 'checking this box'}
+    , you agree to the{' '}
     <Link href="https://rio.gitbook.io/rio-network-docs/Zpr3aRB42rSynxPoQOKe/privacy/terms-of-use">
       Terms of Service
     </Link>{' '}
