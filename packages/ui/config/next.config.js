3c6e83d3fb50c4f8960a09f44a79b19345d10351
tlip <thomas.lipari@gmail.com>
2024-02-23 12:50:33 -0800

    ci: [RIO-175] generate sourcemaps during build and upload to datadog

--- packages/ui/config/next.config.js
+++ packages/ui/config/next.config.js
@@ -1,7 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@rio-monorepo/ui'],
+  productionBrowserSourceMaps: process.env.CI === 'true',
   webpack: (config) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     config.resolve.fallback = { fs: false, net: false, tls: false };
