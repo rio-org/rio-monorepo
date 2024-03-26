28619ea8bad57de6fb69c29eb1a84e76e52c71f6
tlip <thomas.lipari@gmail.com>
2024-03-26 12:08:16 -0700

    fix: internal package import

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -1,15 +1,21 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+import { getConnectionString } from './lib/utils';
+import * as schema from './schema';
 import { ENV } from './config';
-import { getDrizzleClient } from './db';
 
 (async function main() {
-  const { client, db } = getDrizzleClient({
+  const connectionString = getConnectionString({
     host: ENV.DB_HOST,
     port: ENV.DB_PORT,
     user: ENV.DB_USER,
     password: ENV.DB_PASSWORD,
     database: ENV.DB_NAME,
   });
+  const client = postgres(connectionString);
+  const db = drizzle(client, { schema });
   await migrate(db, { migrationsFolder: '../drizzle' });
   await client.end();
 })().catch((error) => {
