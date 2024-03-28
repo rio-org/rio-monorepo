c90e50eec6d89820f7d6bb1242fa8d3cca0728bb
tlip <thomas.lipari@gmail.com>
2024-03-28 12:18:05 -0700

    fix: linting

--- apps/api/src/api/api.module.ts
+++ apps/api/src/api/api.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { DatabaseModule } from '@/libs/common/database/database.module';
-import { SharedConfigModule } from '@/libs/config/shared-config/shared-config.module';
 
 @Module({
   imports: [DatabaseModule],
