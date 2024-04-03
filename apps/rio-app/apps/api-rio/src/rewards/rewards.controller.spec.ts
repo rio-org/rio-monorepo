22e181fea4cd8e7687503f44293a142022e670ca
Bryan Green <bryan@rio.network>
2024-04-03 08:32:48 -0600

    console log redis url for render

--- apps/rio-app/apps/api-rio/src/rewards/rewards.controller.spec.ts
+++ apps/rio-app/apps/api-rio/src/rewards/rewards.controller.spec.ts
@@ -16,7 +16,7 @@ describe('ApiController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      // expect(appController.getTime()).toBe('Hello World!');
     });
   });
 });
