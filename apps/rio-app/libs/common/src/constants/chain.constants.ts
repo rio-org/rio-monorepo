8cd3beca887f284bdab7b9a2c1bd81ee19e218c0
tlip <thomas.lipari@gmail.com>
2024-04-04 12:44:10 -0700

    fix: lint

--- apps/rio-app/libs/common/src/constants/chain.constants.ts
+++ apps/rio-app/libs/common/src/constants/chain.constants.ts
@@ -1,7 +1,5 @@
 export const SUPPORTED_CHAIN_IDS = [1, 5, 17000] as const;
 export const SUPPORTED_CHAIN_NAMES = [
-  'MAINNET',
-  'mainnet',
   'ETHEREUM',
   'ethereum',
   'GOERLI',
--- apps/rio-app/libs/common/src/types/chain.types.ts
+++ apps/rio-app/libs/common/src/types/chain.types.ts
@@ -1,6 +1,5 @@
 export enum CHAIN_ID {
   ETHEREUM = 1,
-  MAINNET = 1,
   GOERLI = 5,
   HOLESKY = 17000,
 }
