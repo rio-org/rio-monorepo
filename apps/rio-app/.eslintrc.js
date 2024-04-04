9628564a6b9829ccf323a1618b1721dff3850479
Bryan Green <bryan@rio.network>
2024-04-04 06:54:28 -0600

    remove config library, prep for multi-chain tasks

--- apps/rio-app/.eslintrc.js
+++ apps/rio-app/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
