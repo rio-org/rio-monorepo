3fd4211dba66e21e0a114f43958af1a70e999e40
Bryan Green <bryan@rio.network>
2024-04-12 12:42:11 -0600

    more dd trace support and config

--- apps/rio-app/libs/common/src/tracing/dd-tracing.ts
+++ apps/rio-app/libs/common/src/tracing/dd-tracing.ts
@@ -4,5 +4,9 @@ import tracer from 'dd-trace';
 tracer.init({
   // https://docs.datadoghq.com/tracing/connect_logs_and_traces/nodejs/
   logInjection: true,
+  env: process.env.ENV,
+  runtimeMetrics: true,
+  profiling: true,
+  reportHostname: true,
 });
 export default tracer;
