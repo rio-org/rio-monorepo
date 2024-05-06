d7c14b2bd7bfb7742078d2047a8fe717a77e51d8
Hamin-rn <166872475+Hamin-rn@users.noreply.github.com>
2024-05-06 09:48:34 -0600

    fix: correct eslint errors

--- packages/sdk/src/utils/constants.ts
+++ packages/sdk/src/utils/constants.ts
@@ -12,12 +12,12 @@ export const WAD = 10n ** 18n;
  * Mapping of token addresses for each supported chain ID.
  */
 export const TOKEN_ADDRESSES_WITH_CHAINS = {
-    1: {
-      rn: '0x3c61297E71E9bB04b9fBfead72A6d3c70e4f1E4A',
-      reeth: '0x', // To do: contract address needs to be added when deployed
-    },
-    17000: {
-      rn: '0x',
-      reeth: '0x84a63F1025b8f1B54dAAd3083A54678E31604973',
-    },
-  };
+  1: {
+    rn: '0x3c61297E71E9bB04b9fBfead72A6d3c70e4f1E4A',
+    reeth: '0x' // To do: contract address needs to be added when deployed
+  },
+  17000: {
+    rn: '0x',
+    reeth: '0x84a63F1025b8f1B54dAAd3083A54678E31604973'
+  }
+};
